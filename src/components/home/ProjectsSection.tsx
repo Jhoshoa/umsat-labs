ac56fb43bb7cf6acc40678a35a8eebeeab21bc6e
jichuta <ichuta.josoe@gmail.com>
2026-03-29 16:44:34 -0400

    carousel implementation for project list home page

--- src/components/home/ProjectsSection.tsx
+++ src/components/home/ProjectsSection.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import styles from '@site/src/pages/index.module.css';
 import { ProjectCard } from './ProjectCard';
 import type { ProjectCardProps } from '@site/src/types/home';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 interface ProjectsSectionProps {
   projects: ProjectCardProps[];
@@ -10,10 +17,41 @@ interface ProjectsSectionProps {
 export const ProjectsSection: React.FC<ProjectsSectionProps> = ({ projects }) => (
   <section className={styles.projectsSection}>
     <h2 className={styles.sectionTitle}>Active Projects</h2>
-    <div className={styles.projectsGrid}>
-      {projects.map((project, idx) => (
-        <ProjectCard key={idx} {...project} />
-      ))}
+    <div className={styles.carouselContainer}>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        loop={projects.length > 3}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+        className={styles.projectsSwiper}
+      >
+        {projects.map((project, idx) => (
+          <SwiperSlide key={idx}>
+            <ProjectCard {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   </section>
 );
